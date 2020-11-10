4ff8aabadc9f769ff31b8a33e02a2e497d023db3
varnier <evarnier.evarnier@la-providence.net>
2020-11-10 16:06:29 +0100

    comit exo1part php

---
+++ PHP/exo1part2.php
@@ -0,0 +1,22 @@
+
+
+
+<?php
+
+
+  
+echo "<tr>"
+        
+echo"<td>SLAM</td>"
+echo"<td> SISR</td>"
+echo"</tr>
+<TR>"
+echo"
+<TD>SLAM 1, SLAM 2</TD>
+<TD>SISR1, SISR2 </TD>
+</TR>"
+
+      
+      return ;
+    }
+?>
---
+++ PHP/exopart2.php
@@ -0,0 +1,26 @@
+
+<?php
+    function tableau() 
+    {
+?>
+    <table border=1>       
+        <tr>
+            
+            <td>prenom</td>
+            <td>nom</td>
+        </tr>
+        <tr>
+            
+            <td>evans</td>
+            <td>varnier</td>
+        </tr>
+        <tr>
+            <td>1</td>
+            <td>2</td>
+        </tr>
+       
+    </table> 
+<?php
+    }
+?>
+

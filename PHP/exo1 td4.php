492d1419f9ee72e3f0c14e511dd915cab53817e2
varnier <evarnier.evarnier@la-providence.net>
2020-11-24 17:05:58 +0100

    comit exo1 td4

---
+++ PHP/exo1 td4.php
@@ -0,0 +1,16 @@
+<?php
+
+
+
+try {
+
+    //code...
+$maBase=new PDO('mysql:host=192.168.64.116; dbname=evans exo2; charset=medecin','root', 'root');
+
+
+
+
+} catch (exception $e) {
+ echo 'probleme de connection'
+ 
+}
